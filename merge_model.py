1d955d451cea6ab4298fb5aa5e30ffadadecf7e6
DU1999 <1362468759@qq.com>
2026-01-13 15:03:37 +0800

    Upload mergeLoRA

---
+++ merge_model.py
@@ -0,0 +1,29 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import PeftModel
+
+base_model_path = "Qwen_Qwen3-4B-Instruct-2507"
+lora_ckpt_path = "checkpoint"  
+merged_model_path = "RES-RAG-MODEL" 
+
+# 1. 加载基座模型
+model = AutoModelForCausalLM.from_pretrained(
+    base_model_path,
+    torch_dtype=torch.bfloat16, 
+    device_map="cuda" 
+)
+
+# Loading LoRA adapter
+model = PeftModel.from_pretrained(model, lora_ckpt_path)
+
+# Merge the LoRA weights back into the base model and uninstall the PEFT wrapper.
+model = model.merge_and_unload()
+
+# Save the merged model.
+model.save_pretrained(merged_model_path)
+
+# Synchronously save the tokenizer.
+tokenizer = AutoTokenizer.from_pretrained(base_model_path, use_fast=True)
+tokenizer.save_pretrained(merged_model_path)
+
+
